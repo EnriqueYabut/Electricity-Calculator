bd6247063d3d399c6c080ce50e97c4aefa7a69e2
EnriqueYabut <joselito.yabut090521@my.lsgh.edu.ph>
2021-04-27 17:56:04 +0800

    2 files modified, 1 added

--- pages/home/home.ts
+++ pages/home/home.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-
+ 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+ selector: 'page-home',
+ templateUrl: 'home.html',
+ styleUrls: ['home.css']
+ 
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
-
-  }
-
+ 
+ constructor(public navCtrl: NavController) {
+ 
+ }
+ 
 }
